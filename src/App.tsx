6c9b442bb3ff73da63f0cccef14f9cbcd89eea77
Marcio Tiene <marciorft@gmail.com>
2021-02-03 18:20:18 -0300

    adding react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import Home from './pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' component={Home} exact />
+        </Switch>
+      </BrowserRouter>
       <Home />
     </>
   );
