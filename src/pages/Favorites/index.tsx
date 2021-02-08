5b22f8f959a2803d3f35078b12ddb95c0d1aea99
Marcio Tiene <marciorft@gmail.com>
2021-02-08 10:01:42 -0300

    fixing favorites page 1 show

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -6,19 +6,20 @@ import BreadCrumb from '../../components/BreadCrumb';
 import { pagesNumberContructor } from '../../components/PageTurners';
 import SearchContent from '../../components/SearchContent';
 import { favorite } from '../../services/localStorageHandler';
+import { dataInitialState } from '../../services/utils';
 
 const Favorites: React.FC = () => {
   const history = useHistory();
   const { id } = useParams<Record<string, string | undefined>>();
   const page = Number(id);
-  const initialSlice = page * 10 - 11;
+  const initialSlice = page * 10 - 10;
   const finalSlice = initialSlice + 9;
-  const data = favorite.slice(initialSlice, finalSlice) || [];
+  const data = favorite.slice(initialSlice, finalSlice);
 
-  if (data.length === 0) history.push('/error');
+  if (data.length === 0 || data[0].id === dataInitialState[0].id)
+    history.push('/error');
 
   const totalHits = favorite.length;
-  console.log(id);
 
   const pages = pagesNumberContructor(page, totalHits);
 
